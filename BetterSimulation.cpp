69742fb1cf512a540e36bcad09a0b240aec923c9
Dhanush123 <dhanush.patel@ymail.com>
2016-11-19 17:11:52 -0800

    finished everything finally
    done now

---
+++ BetterSimulation.cpp
@@ -0,0 +1,191 @@
+// Programmer: Dhanush Patel
+// Programmer's ID: 1553428
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <queue>
+using namespace std;
+
+#include <cmath>
+#include <cstdlib>
+
+#include "PriorityQueue.h"
+#include "DynamicArray.h"
+
+struct Customer{
+  char ID;
+  int arrivalT;
+};
+
+struct ServerInfo{
+  bool status;
+  Customer cust;
+};
+
+struct ServiceEvent{
+  int serverIndex;
+  int endT;
+};
+
+int getRandomNumberOfArrivals(double);
+char genID(char&);
+int randTimeAdd(int, int);
+
+bool operator<(const ServiceEvent& a, const ServiceEvent& b){
+  return a.endT >= b.endT;
+}
+
+int main(){
+  srand(time(0));
+  rand();
+  cout << "Programmer: Dhanush Patel\n";
+  cout << "Programmer's ID: 1553428\n";
+  cout << "File: " << __FILE__ << "\n\n";
+  int numServers = 0, waitMaxLength = 0, minServTime = 0, maxServTime = 0, clockStopTime = 0;
+  double avgArrivalRate = 0.0;
+  char curAlpha = 'A';
+
+  ifstream fin;
+  fin.open("simulation.txt");
+  int switchCount = 0;
+  while (fin.good()){
+    string input;
+    getline(fin, input);
+    switch (switchCount){
+      case 0:
+        numServers = atoi(input.c_str());
+        cout << "number of servers:     " << numServers << endl;
+        break;
+      case 1:
+        avgArrivalRate = atof(input.c_str());
+        break;
+      case 2:
+        waitMaxLength = atoi(input.c_str());
+        cout << "maximum queue length:  " << waitMaxLength << endl;
+        break;
+      case 3:
+        minServTime = atoi(input.c_str());
+        cout << "minimum service time:  " << minServTime << " minutes" << endl;
+        break;
+      case 4:
+        maxServTime = atoi(input.c_str());
+        cout << "maximum service time:  " << maxServTime << " minutes" << endl;
+        break;
+      case 5:
+        clockStopTime = atoi(input.c_str());
+        cout << "customer arrival rate: " << avgArrivalRate << " per minute, for " << clockStopTime << " minutes"
+             << endl;
+        break;
+      default:
+        throw ("UH OH - PROBLEM");
+    }
+    switchCount++;
+  }
+
+  queue<Customer> custQ;
+  DynamicArray<ServerInfo> servers;
+  PriorityQueue<ServiceEvent> serviceInfo;
+
+  // the clock time loop
+  for (int time = 0; ; time++){
+    // handle all services scheduled to complete at this clock time
+    while ((serviceInfo.top().endT == time) && !serviceInfo.empty()){
+      servers[serviceInfo.top().serverIndex].status = false;
+      serviceInfo.pop();
+    }
+
+    // handle new arrivals -- can be turned away if wait queue is at maximum length!
+    if (time < clockStopTime){
+      int numArrive = getRandomNumberOfArrivals(avgArrivalRate);
+      for (int i = 0; i < numArrive; i++){
+        if (custQ.size() < waitMaxLength){
+          Customer c;
+          c.ID = genID(curAlpha);
+          c.arrivalT = time;
+          custQ.push(c);
+        }
+      }
+    }
+
+    // for idle servers, move customer from wait queue and begin service
+    for (int i = 0; i < numServers; i++){
+      if (!servers[i].status && !custQ.empty()){
+        servers[i].cust = custQ.front();
+        custQ.pop();
+        servers[i].status = true;
+        ServiceEvent temp = {i, time + randTimeAdd(minServTime, maxServTime)};
+        serviceInfo.push(temp);
+      }
+    }
+
+    // output event list
+    cout << "server | time for end-of-service\n" << "------ + -----------------------\n";
+    for (PriorityQueue<ServiceEvent> temp = serviceInfo; !temp.empty(); temp.pop()){
+      cout.width(4);
+      cout << temp.top().serverIndex << "   |";
+      cout.width(12);
+      cout << temp.top().endT << endl;
+    }
+
+    //output the summary
+    //output the current time
+    //output a visual representation of the servers and the wait queue
+    cout << "\nTime: " << time << endl;
+    cout << "---------------------------" << endl;
+    cout << "server now serving wait queue" << endl;
+    cout << "------ ----------- ----------" << endl;
+    for (int i = 0; i < numServers; i++){
+      string show = " ";
+      if (servers[i].status){
+        show = servers[i].cust.ID;
+      }
+      cout << setw(2) << i << setw(10) << show;
+      if (i == 0){
+        queue<Customer> tempQ = custQ;
+        cout << setw(10);
+        while (!tempQ.empty()) {
+          cout << tempQ.front().ID;
+          tempQ.pop();
+        }
+      }
+      cout << endl;
+    }
+
+    int numIdle = 0;
+    for (int i = 0; i < numServers; i++){
+      if (!servers[i].status) {
+        numIdle++;
+      }
+    }
+    if (numIdle == numServers && time >= clockStopTime){
+      break;
+    }
+    do{
+      cout << '\n' << "Press ENTER to continue...";
+    } while (cin.get() != '\n');
+  }
+}
+
+int randTimeAdd(const int a, const int b){
+  return a + (rand() % b);
+}
+
+char genID(char& curAlpha){
+  if(curAlpha == 'Z'){
+    curAlpha = 'A';
+    return 'Z';
+  }
+  return curAlpha++;
+}
+
+int getRandomNumberOfArrivals(double averageArrivalRate){
+  int arrivals = 0;
+  double probOfnArrivals = exp(-averageArrivalRate);
+  for (double randomValue = (double)rand() / RAND_MAX;
+       (randomValue -= probOfnArrivals) > 0.0;
+       probOfnArrivals *= averageArrivalRate / static_cast<double>(++arrivals));
+  return arrivals;
+}
+
---
+++ PriorityQueue.pop.cpp
@@ -0,0 +1,80 @@
+//Programmer: Dhanush Patel
+//Programmer ID: 1553428
+
+#include <iostream> // for cout, ios, and endl
+#include <string> // for string
+using namespace std;
+
+#include <cassert> // for assert
+#include <cmath> // for log and pow
+#include <cstdlib> // for srand and rand
+#include <ctime> // for clock(), clock_t, time, and CLOCKS_PER_SEC
+
+#include "PriorityQueue.h"
+
+int main(){
+  srand(time(0)); rand(); // seed the random number generator (in case you need it)
+
+  cout << "Programmer: Dhanush Patel\n";
+  cout << "Programmer's ID: 1553428\n";
+  cout << "File: " << __FILE__ << endl;
+
+  // programmer customization go here
+  int n = 100000; // THE STARTING PROBLEM SIZE (MAX 250 MILLION)
+  const int nReps = n / 100; // one percent of starting n
+
+  cout.setf(ios::fixed);
+  cout.precision(4);
+  double elapsedSecondsNorm = 0;
+  for(int cycle = 0; cycle < 4; cycle++, n*= 2){
+    // problem setup goes here -- create a data structure of size n
+    PriorityQueue<int> timeQ(n * 2);
+    
+    for(int i = 0; i < n; i++){
+      timeQ.push(i);
+    }
+
+    // assert that n is the size of the data structure if applicable
+    assert(timeQ.size() == n);
+
+    //making 1 more...a copy
+    PriorityQueue<int> timeQ2 = timeQ;
+    while(!timeQ2.empty()){ //test pop for 2nd  and seeing if structure is legit
+      int temp = timeQ2.top();
+      timeQ2.pop();
+      assert(temp >= timeQ2.top());
+    }
+
+    // start the timer, do something, and stop the timer
+    clock_t startTime = clock();
+    for(int rep = 0; rep < nReps; rep++){ //now poping the 1st PQ for timing purposes
+      timeQ.pop();
+    }
+
+    // do something where n is the "size" of the problem
+    clock_t endTime = clock();
+
+    // validation block -- assure that process worked if applicable
+    assert(timeQ.size() == n - nReps);
+    timeQ2 = timeQ;
+
+    while(!timeQ2.empty()){ // making sure 2nd PQ is still legit
+      int temp = timeQ2.top();
+      timeQ2.pop();
+      assert(temp >= timeQ2.top());
+    }
+
+    // compute timing results
+    double elapsedSeconds = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+    double factor = pow(2.0, double(cycle));
+    if(cycle == 0) elapsedSecondsNorm = elapsedSeconds;
+    double expectedSecondsLog = log(double(n)) / log(n / factor) * elapsedSecondsNorm;
+
+    // reporting block
+    cout << elapsedSeconds;
+    if(cycle == 0) cout << " (expected)";
+    else cout << " (expected " << elapsedSecondsNorm << " to " << expectedSecondsLog << ')';
+    cout << " for n=" << n << endl;
+  }
+  return 0;
+}
